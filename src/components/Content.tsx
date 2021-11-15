203bce6423118b0de706d22d7b58ee7272ba159b
Gabriel Fernandes <profissional.gabrielg@gmail.com>
2021-11-14 23:43:56 -0300

    Resolução 2 Desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MovieProprier {
+  movies: MovieProps[];
+  genreSelected: string;
+}
+
+
+export function Content(props: MovieProprier) {
+  return ( <div className="container">
+  <header>
+    <span className="category">Categoria:<span> {props.genreSelected}</span></span>
+  </header>
+  <main>
+    <div className="movies-list">
+      {props.movies.map(prop => (
+        <MovieCard key ={prop.imdbID} title={prop.Title} poster={prop.Poster} runtime={prop.Runtime} rating={prop.Ratings[0].Value} />
+      ))}
+    </div>
+  </main>
+</div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+
+interface GenreResponseItemProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface GenreListProps {
+  genres: GenreResponseItemProps[];
+  escolherCategoria (id: number) : any;
+  idSelect: number;
+}
+
+
+export function SideBar(props: GenreListProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+    {props.genres.map(prop => (
+          <Button
+            key={String(prop.id)}
+            title={prop.title}
+            iconName={prop.name}
+            onClick={() => props.escolherCategoria(prop.id)}
+            selected={props.idSelect === prop.id} />
+    ))}
+     </div>
+    </nav>
+  )
 }
